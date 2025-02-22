785dd17d4a89ca79935c8beb71c926c18ea575fc
ArunkumarB <arun030@gmail.com>
2025-02-22 16:02:03 +0530

    updated the new database details

--- config/db_connect.php
+++ config/db_connect.php
@@ -2,7 +2,7 @@
 $hostname='127.0.0.1';
 $db_username='root';
 $db_password='';
-$db_name='rental_db';
+$db_name='Building_Maintenance';
 
 $con=mysqli_connect($hostname,$db_username,$db_password,$db_name);
 
@@ -13,8 +13,8 @@ if (mysqli_connect_errno()) {
 date_default_timezone_set("Asia/Kolkata");
 $local = array("::1", "127.0.0.1");
 if ( in_array($_SERVER['REMOTE_ADDR'], $local) ) {
-	$basePath = $_SERVER['DOCUMENT_ROOT']."/room/";
-	$baseUrl =  'http://'.$_SERVER['SERVER_NAME'].'/Room/';
+	$basePath = $_SERVER['DOCUMENT_ROOT']."/Home/";
+	$baseUrl =  'http://'.$_SERVER['SERVER_NAME'].'/Home/';
 }else {
 	$basePath = $_SERVER['DOCUMENT_ROOT']."/";
 	$baseUrl =  'http://'.$_SERVER['SERVER_NAME'].'/';
