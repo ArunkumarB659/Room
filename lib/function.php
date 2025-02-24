02d9a91af410b3597da4eadbcbb0268a8da14583
ArunkumarB <arun030@gmail.com>
2025-02-24 17:21:53 +0530

    updated function name and table coulmn names and port variable names in lib/function.php sql query function page

--- lib/function.php
+++ lib/function.php
@@ -4,15 +4,16 @@ include("../config/db_connect.php");
 include $_SERVER["DOCUMENT_ROOT"]."/home/config/db_connect.php";
 class RentalManager {
 	
-function addRental($rNumber, $tName, $amount, $month, $ebBill, $waterFront1, $waterBack1, $notes) {
+function addTenantDetails($Tenant_name, $Room_number, $Mobile_number, $Permanent_address, $Local_address, $Aadhaar_number) {
 	global $con;
     $response = array();
-    $sql = "INSERT INTO rental_details (room_number, tenant_name, rent_amount, month, eb_bill, water_front_1, water_back_1, notes) VALUES ('$rNumber', '$tName', '$amount', '$month', '$ebBill', '$waterFront1', '$waterBack1', '$notes')";
+    $sql = "INSERT INTO Tenant_details (Tenant_name, Room_number, Mobile_number, Permanent_address, Local_address, Aadhaar_number) VALUES ('$Tenant_name', '$Room_number', '$Mobile_number', '$Permanent_address', '$Local_address', '$Aadhaar_number')";
     if (mysqli_query($con,$sql)) {
       $response['status'] = 'success';
-      $response['message'] = 'Rental details added successfully.';
+      $response['message'] = 'Tenant details added successfully.';
     } else {
       $response['message'] = 'Database insertion failed.';
+	  echo "Error: " . mysqli_error($con); // This line will display the error in network response
     }
     return $response;
   }
