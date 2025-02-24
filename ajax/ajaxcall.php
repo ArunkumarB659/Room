835dd8c51c0833dc8b2a0d56bf0e39bea06238f2
ArunkumarB <arun030@gmail.com>
2025-02-24 16:39:21 +0530

    updated ajax/ajaxcall.php sql function call name and the variables

--- ajax/ajaxcall.php
+++ ajax/ajaxcall.php
@@ -5,8 +5,8 @@ include("../lib/function.php");
 $RentalManager = new RentalManager();
 $response = array('status' => 'error');
 
-if (isset($_POST['action']) && $_POST['action'] === 'submitRentalDetails') {
-  $response = $RentalManager->addRental($_POST['rNumber'], $_POST['tName'], $_POST['amount'], $_POST['month'], $_POST['ebBill'], $_POST['waterFront1'], $_POST['waterBack1'], $_POST['notes'] );
+if (isset($_POST['action']) && $_POST['action'] === 'submitTenantDetails') {
+  $response = $RentalManager->addTenantDetails($_POST['Tenant_name'], $_POST['Room_number'], $_POST['Mobile_number'], $_POST['Permanent_address'], $_POST['Local_address'], $_POST['Aadhaar_number']);
 } else {
   $response['message'] = 'Invalid action.';
 }
