81737a9d8b04d7cbf7ee47f487199aa8e44dda4b
ArunkumarB <arun030@gmail.com>
2025-03-03 10:19:16 +0530

    add ajax code for rental details form data serialize and tranfer the data SQL function

--- ajax/ajaxcall.php
+++ ajax/ajaxcall.php
@@ -7,9 +7,18 @@ $response = array('status' => 'error');
 
 if (isset($_POST['action']) && $_POST['action'] === 'submitTenantDetails') {
   $response = $RentalManager->addTenantDetails($_POST['Tenant_name'], $_POST['Room_number'], $_POST['Mobile_number'], $_POST['Permanent_address'], $_POST['Local_address'], $_POST['Aadhaar_number']);
-} else {
+} else
+{
   $response['message'] = 'Invalid action.';
 }
 
-echo json_encode($response);
+if (isset($_POST['action']) && $_POST['action'] === 'SubmitAddrentDetails') {
+  $response = $RentalManager->addRentDetails($_POST['month_year'],$_POST['rent_room_number'], $_POST['water_front'], $_POST['water_back'], $_POST['eb_bill'], $_POST['maintenance'], $_POST['Notes']);
+} else
+{
+  $response['message'] = 'Invalid action.';
+}
+
+
+echo json_encode($response); 
 ?>
